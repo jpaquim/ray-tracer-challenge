2f1043f0d570dd9dcba66f7ba3dd7ddb304a6139
jpaquim <joaopaquim@gmail.com>
2022-06-19 22:00:02 +0100

    Add example projectile cannon

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
-
 const Tuple = [4]f64;
 
 fn isPoint(tuple: Tuple) bool {
@@ -317,4 +313,34 @@ test "The cross product of two vectors" {
     const b = vector(2, 3, 4);
     try std.testing.expectEqual(vector(-1, 2, -1), cross(a, b));
     try std.testing.expectEqual(vector(1, -2, 1), cross(b, a));
+}
+
+const Projectile = struct {
+    position: Tuple,
+    velocity: Tuple,
+};
+
+const Environment = struct {
+    gravity: Tuple,
+    wind: Tuple,
+};
+
+fn tick(env: Environment, proj: Projectile) Projectile {
+    const position = add(proj.position, proj.velocity);
+    const velocity = add(add(proj.velocity, env.gravity), env.wind);
+    return Projectile{ .position = position, .velocity = velocity };
+}
+
+pub fn main() anyerror!void {
+    const stdout = std.io.getStdOut().writer();
+
+    var p = Projectile{ .position = point(0, 1, 0), .velocity = normalize(vector(1, 1, 0)) };
+    const e = Environment{ .gravity = vector(0, -0.1, 0), .wind = vector(-0.01, 0, 0) };
+
+    var ticks: usize = 0;
+    while (p.position[1] > 0) : (ticks += 1) {
+        p = tick(e, p);
+        try stdout.print("position: {any}\n", .{p.position});
+    }
+    try stdout.print("ticks: {}\n", .{ticks});
 }
