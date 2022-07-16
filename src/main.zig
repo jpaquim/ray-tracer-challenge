346bd31a9ff9a869488d8833b31595e01ebc8b5e
jpaquim <joaopaquim@gmail.com>
2022-07-16 19:57:42 +0100

    Add matrix inverse calculation

--- src/main.zig
+++ src/main.zig
@@ -1115,3 +1115,150 @@ test "Testing a noninvertible matrix for invertibility" {
     try std.testing.expectEqual(@as(f64, 0), determinant(4, A));
     try std.testing.expect(!is_invertible(4, A));
 }
+
+fn inverse(comptime n: comptime_int, m: Matrix(n)) !Matrix(n) {
+    if (!is_invertible(n, m)) return error.NotInvertible;
+
+    var result: Matrix(n) = undefined;
+
+    var i: usize = 0;
+    while (i < 4) : (i += 1) {
+        var j: usize = 0;
+        while (j < 4) : (j += 1) {
+            const c = cofactor(n, m, i, j);
+
+            result[j][i] = c / determinant(n, m);
+        }
+    }
+    return result;
+}
+
+fn expectMatrixApproxEqAbs(expected: Matrix(4), actual: Matrix(4)) !void {
+    var i: usize = 0;
+    while (i < 4) : (i += 1) {
+        var j: usize = 0;
+        while (j < 4) : (j += 1) {
+            try std.testing.expectApproxEqAbs(expected[i][j], actual[i][j], epsilon);
+        }
+    }
+}
+
+// Scenario: Calculating the inverse of a matrix
+//   Given the following 4x4 matrix A:
+//       | -5 |  2 |  6 | -8 |
+//       |  1 | -5 |  1 |  8 |
+//       |  7 |  7 | -6 | -7 |
+//       |  1 | -3 |  7 |  4 |
+//     And B ← inverse(A)
+//   Then determinant(A) = 532
+//     And cofactor(A, 2, 3) = -160
+//     And B[3,2] = -160/532
+//     And cofactor(A, 3, 2) = 105
+//     And B[2,3] = 105/532
+//     And B is the following 4x4 matrix:
+//       |  0.21805 |  0.45113 |  0.24060 | -0.04511 |
+//       | -0.80827 | -1.45677 | -0.44361 |  0.52068 |
+//       | -0.07895 | -0.22368 | -0.05263 |  0.19737 |
+//       | -0.52256 | -0.81391 | -0.30075 |  0.30639 |
+test "Calculating the inverse of a matrix" {
+    const A = Matrix(4){
+        .{ -5, 2, 6, -8 },
+        .{ 1, -5, 1, 8 },
+        .{ 7, 7, -6, -7 },
+        .{ 1, -3, 7, 4 },
+    };
+    const B = try inverse(4, A);
+    try std.testing.expectEqual(@as(f64, 532), determinant(4, A));
+    try std.testing.expectEqual(@as(f64, -160), cofactor(4, A, 2, 3));
+    try std.testing.expectEqual(@as(f64, -160) / 532, B[3][2]);
+    try std.testing.expectEqual(@as(f64, 105), cofactor(4, A, 3, 2));
+    try std.testing.expectEqual(@as(f64, 105) / 532, B[2][3]);
+    try expectMatrixApproxEqAbs(Matrix(4){
+        .{ 0.21805, 0.45113, 0.24060, -0.04511 },
+        .{ -0.80827, -1.45677, -0.44361, 0.52068 },
+        .{ -0.07895, -0.22368, -0.05263, 0.19737 },
+        .{ -0.52256, -0.81391, -0.30075, 0.30639 },
+    }, B);
+}
+
+// Scenario: Calculating the inverse of another matrix
+//   Given the following 4x4 matrix A:
+//     |  8 | -5 |  9 |  2 |
+//     |  7 |  5 |  6 |  1 |
+//     | -6 |  0 |  9 |  6 |
+//     | -3 |  0 | -9 | -4 |
+//   Then inverse(A) is the following 4x4 matrix:
+//     | -0.15385 | -0.15385 | -0.28205 | -0.53846 |
+//     | -0.07692 |  0.12308 |  0.02564 |  0.03077 |
+//     |  0.35897 |  0.35897 |  0.43590 |  0.92308 |
+//     | -0.69231 | -0.69231 | -0.76923 | -1.92308 |
+test "Calculating the inverse of another matrix" {
+    const A = Matrix(4){
+        .{ 8, -5, 9, 2 },
+        .{ 7, 5, 6, 1 },
+        .{ -6, 0, 9, 6 },
+        .{ -3, 0, -9, -4 },
+    };
+    try expectMatrixApproxEqAbs(Matrix(4){
+        .{ -0.15385, -0.15385, -0.28205, -0.53846 },
+        .{ -0.07692, 0.12308, 0.02564, 0.03077 },
+        .{ 0.35897, 0.35897, 0.43590, 0.92308 },
+        .{ -0.69231, -0.69231, -0.76923, -1.92308 },
+    }, try inverse(4, A));
+}
+
+// Scenario: Calculating the inverse of a third matrix
+//   Given the following 4x4 matrix A:
+//     |  9 |  3 |  0 |  9 |
+//     | -5 | -2 | -6 | -3 |
+//     | -4 |  9 |  6 |  4 |
+//     | -7 |  6 |  6 |  2 |
+//   Then inverse(A) is the following 4x4 matrix:
+//     | -0.04074 | -0.07778 |  0.14444 | -0.22222 |
+//     | -0.07778 |  0.03333 |  0.36667 | -0.33333 |
+//     | -0.02901 | -0.14630 | -0.10926 |  0.12963 |
+//     |  0.17778 |  0.06667 | -0.26667 |  0.33333 |
+test "Calculating the inverse of a third matrix" {
+    const A = Matrix(4){
+        .{ 9, 3, 0, 9 },
+        .{ -5, -2, -6, -3 },
+        .{ -4, 9, 6, 4 },
+        .{ -7, 6, 6, 2 },
+    };
+    try expectMatrixApproxEqAbs(Matrix(4){
+        .{ -0.04074, -0.07778, 0.14444, -0.22222 },
+        .{ -0.07778, 0.03333, 0.36667, -0.33333 },
+        .{ -0.02901, -0.14630, -0.10926, 0.12963 },
+        .{ 0.17778, 0.06667, -0.26667, 0.33333 },
+    }, try inverse(4, A));
+}
+
+// Scenario: Multiplying a product by its inverse
+//   Given the following 4x4 matrix A:
+//       |  3 | -9 |  7 |  3 |
+//       |  3 | -8 |  2 | -9 |
+//       | -4 |  4 |  4 |  1 |
+//       | -6 |  5 | -1 |  1 |
+//     And the following 4x4 matrix B:
+//       |  8 |  2 |  2 |  2 |
+//       |  3 | -1 |  7 |  0 |
+//       |  7 |  0 |  5 |  4 |
+//       |  6 | -2 |  0 |  5 |
+//     And C ← A * B
+//   Then C * inverse(B) = A
+test "Multiplying a product by its inverse" {
+    const A = Matrix(4){
+        .{ 3, -9, 7, 3 },
+        .{ 3, -8, 2, -9 },
+        .{ -4, 4, 4, 1 },
+        .{ -6, 5, -1, 1 },
+    };
+    const B = Matrix(4){
+        .{ 8, 2, 2, 2 },
+        .{ 3, -1, 7, 0 },
+        .{ 7, 0, 5, 4 },
+        .{ 6, -2, 0, 5 },
+    };
+    const C = matrixMult(A, B);
+    try expectMatrixApproxEqAbs(A, matrixMult(C, try inverse(4, B)));
+}
